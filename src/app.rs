2b243531b31e773e04e0129c32950cbeaa2cb618
Sakaki Aruka <99568054+Sakaki-Aruka@users.noreply.github.com>
2024-05-11 02:25:32 +0900

    update

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,11 @@
-use std::io::{Stdout, stdout};
-use crossterm::cursor::{MoveTo, MoveToNextLine};
-use crossterm::execute;
+use std::io::{Stdout, stdout, Write};
+use crossterm::cursor::{Hide, MoveLeft, MoveRight, MoveTo, MoveToNextLine, MoveToPreviousLine, Show};
+use crossterm::{execute, queue};
 use crossterm::terminal::{Clear, ClearType, ScrollDown, ScrollUp};
 use crossterm::style::Print;
 use ratatui::backend::CrosstermBackend;
 use ratatui::Terminal;
+use unicode_width::*;
 
 pub struct App {
     pub chars : Vec<char>,
@@ -31,24 +32,28 @@ impl App {
     }
 
     pub fn render_after_cursor(&mut self, terminal: &mut Terminal<CrosstermBackend<Stdout>>, move_back: bool) {
-        execute!(stdout(), Clear(ClearType::FromCursorDown));
-        if self.index as usize + 1 >= self.chars.len() { return };
         let (x, y, width, height): (u16, u16, u16, u16) = Self::get_sizes(terminal);
+        execute!(stdout(), Clear(ClearType::FromCursorDown), MoveTo(x, y));
+        if self.index as usize >= self.chars.len() { return };
+        queue!(stdout(), Hide); // cursor hide
+        let mut add: usize = 0;
         loop {
-            if self.index == self.chars.len() as i64 - 1 { break };
-            let (xx, yy, _, _): (u16, u16, u16, u16) = Self::get_sizes(terminal);
-            let c: char = *self.chars.get(self.index as usize + 1).unwrap();
+            let (xx, yy, _, _) = Self::get_sizes(terminal);
+            let index: usize = self.index as usize + add;
+            let x_last: bool = xx == width - 1;
+            let y_last: bool = yy == height - 1;
+            if index >= self.chars.len() - 1 { break };
+            let c: char = *self.chars.get(index).unwrap();
+            if y_last && (x_last || xx + c.width_cjk().unwrap() as u16 >= width) { break };
+            add += 1;
             if c == '\n' {
-                if yy == height - 1 { break };
-                self.index += 1;
-                execute!(stdout(), MoveToNextLine(1));
-                continue;
-            }
-            if xx == width - 1 && yy == height - 1 { break };
-            execute!(stdout(), Print(&c));
-            self.index += 1;
+                queue!(stdout(), MoveToNextLine(1), Clear(ClearType::CurrentLine));
+                continue
+            } else { queue!(stdout(), Print(c)); }
         }
-        if move_back { execute!(stdout(), MoveTo(x, y)); }
+        if move_back { queue!(stdout(), MoveTo(x, y)); }
+        queue!(stdout(), Show);
+        stdout().flush().unwrap();
     }
 
     pub fn enter(&mut self, terminal: &mut Terminal<CrosstermBackend<Stdout>>) {
@@ -62,19 +67,15 @@ impl App {
             return;
         }
 
-        if bottom {
-            execute!(stdout(), ScrollUp(1));
-            if !has_more {
-                execute!(stdout(), MoveToNextLine(1));
-                return;
-            }
-            self.render_after_cursor(terminal, has_more);
-        }
+        if bottom { execute!(stdout(), ScrollUp(1)); }
+        execute!(stdout(), Clear(ClearType::FromCursorDown), MoveToNextLine(1));
+        if !has_more { return; }
+        self.render_after_cursor(terminal, true);
     }
 
     pub fn input(&mut self, c: &char, terminal: &mut Terminal<CrosstermBackend<Stdout>>) {
         let (x, y, width, height) = Self::get_sizes(terminal);
-        self.chars.insert(if self.index == -1 { 0 } else {self.index as usize}, *c);
+        self.chars.insert(if self.index == -1 { 0 } else { self.index as usize }, *c);
         self.index += 1;
 
         let insert: bool = self.index != self.chars.len() as i64 - 1;
@@ -83,20 +84,84 @@ impl App {
         if !scroll && !insert{
             execute!(stdout(), Print(*c));
         } else if scroll {
-            execute!(stdout(), ScrollUp(0), Print(" "), Print(*c));
+            execute!(stdout(), ScrollUp(0), Print(*c));
             self.render_after_cursor(terminal, insert);
         } else {
-            execute!(stdout(), Print(" "), Print(*c));
+            execute!(stdout(), Print(*c));
             self.render_after_cursor(terminal, true);
         }
     }
 
     pub fn move_right(&mut self, terminal: &mut Terminal<CrosstermBackend<Stdout>>) {
-        //
+        let (x, y, width, height): (u16, u16, u16, u16) = Self::get_sizes(terminal);
+        if self.index >= self.chars.len() as i64 - 1 { return };
+        let c: char = *self.chars.get(self.index as usize).unwrap();
+        let y_last: bool = y == height - 1;
+        let x_last: bool = x == width - 1;
+
+        self.index += 1;
+        if !y_last && !x_last {
+            if c != '\n' {
+                execute!(stdout(), MoveRight(1));//Print(c));
+            } else {
+                // '\n'
+                execute!(stdout(), MoveToNextLine(1));
+            }
+        } else if x_last {
+            if y_last {
+                execute!(stdout(), ScrollUp(1));
+                self.render_after_cursor(terminal, true);
+            } else {
+                execute!(stdout(), MoveToNextLine(1));
+            }
+        }
+    }
+
+    fn get_above_line_chars(&mut self, terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Option<Vec<char>> {
+        // from the cursor to the latest \n.
+        // e.g.) "hello\n world" -> " world"
+        if self.index <= 0 { return None };
+        let mut result: Vec<char> = Vec::new();
+        let mut count: usize = 0;
+        let (x, y, width, height): (u16, u16, u16, u16) = Self::get_sizes(terminal);
+        for i in (0..self.index - 1).rev() {
+            let c: char = *self.chars.get(i as usize).unwrap();
+            if c == '\n' || count + c.width_cjk().unwrap() >= width as usize { break };
+            result.push(c);
+            count += c.width_cjk().unwrap();
+        }
+        if result.is_empty() { return None };
+        result.reverse();
+        Some(result)
     }
 
     pub fn move_left(&mut self, terminal: &mut Terminal<CrosstermBackend<Stdout>>) {
-        //
+        let (x, y, width, height): (u16, u16, u16, u16) = Self::get_sizes(terminal);
+        if self.index <= 0 {
+            if self.index == 0 { execute!(stdout(), MoveLeft(1)); }
+            return
+        };
+        let highest: bool = y == height - 1;
+        let most_left: bool = x == 0;
+
+        self.index -= 1;
+        if highest && most_left {
+            // scroll up
+            let chars: Option<Vec<char>> = self.get_above_line_chars(terminal);
+            if chars.is_none() { return };
+            execute!(stdout(), ScrollDown(1), MoveToPreviousLine(1));
+            for c in chars.unwrap() {
+                execute!(stdout(), Print(c));
+            }
+        } else if most_left {
+            let above_line: Option<Vec<char>> = self.get_above_line_chars(terminal);
+            let x: u16 = if above_line.is_none() { 0 } else { above_line.unwrap().len() as u16 };
+            let y: u16 = y - 1;
+            execute!(stdout(), MoveTo(x, y));
+        } else {
+            // !most_left
+            execute!(stdout(), MoveLeft(1));
+        }
     }
 
     pub fn page_up(&mut self, terminal : &mut Terminal<CrosstermBackend<Stdout>>) {
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 mod app;
 
 use std::io::{stdout, Result, Stdout};
-use crossterm::cursor::{MoveLeft, MoveRight, MoveTo, MoveToNextLine, MoveToPreviousLine};
+use std::panic::catch_unwind;
+use crossterm::cursor::{MoveTo};
 use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers, read};
 use crossterm::{event, execute};
 use crossterm::style::Print;
-use crossterm::terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode, EnableLineWrap, EnterAlternateScreen, LeaveAlternateScreen, ScrollDown, ScrollUp};
+use crossterm::terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode, EnableLineWrap, EnterAlternateScreen, LeaveAlternateScreen};
 use ratatui::prelude::CrosstermBackend;
 use ratatui::Terminal;
 use crate::app::App;
@@ -18,24 +19,11 @@ fn main() -> Result<()> {
 
     let mut app: App = App::new();
 
-    let mut terminal_width_buffer: u16 = terminal.size()?.width;
-    let mut terminal_height_buffer: u16 = terminal.size()?.height;
-
-
-    // //debug
-    // for i in 1..(terminal_width_buffer * (terminal_height_buffer-1)) {
-    //     execute!(stdout(), Print("A"));
-    // }
-
     'main_loop: loop {
         while let Event::Key(KeyEvent {code, modifiers, kind, ..}) = event::read()? {
             if modifiers == KeyModifiers::CONTROL && code == KeyCode::Char('q') {
                 break 'main_loop;
             }
-
-            let (x, y) : (u16, u16) = terminal.get_cursor().unwrap();
-            let width : u16 = terminal.size()?.width;
-            let height : u16 = terminal.size()?.height;
 
             match code {
                 KeyCode::Char(c) => {
@@ -46,90 +34,17 @@ fn main() -> Result<()> {
                     app.page_down(&mut terminal);
                 }, KeyCode::Enter => {
                     app.enter(&mut terminal);
+                }, KeyCode::Right => {
+                    app.move_right(&mut terminal);
+                }, KeyCode::Left => {
+                    app.move_left(&mut terminal);
                 },
                 _ => ()
             }
         }
     }
 
-    //
-    // 'main_loop: loop {
-    //     while let Event::Key(KeyEvent { code, modifiers, kind, .. }) = event::read()? {
-    //         if modifiers == KeyModifiers::CONTROL && code == KeyCode::Char('q') {
-    //             break 'main_loop;
-    //         }
-    //
-    //         let (x, y) = terminal.get_cursor()?;
-    //         let x_last: bool = x == terminal_width_buffer - 1;
-    //         let y_last: bool = y == terminal_height_buffer - 1;
-    //         let width : u16 = terminal.size()?.width;
-    //         let height : u16 = terminal.size()?.height;
-    //
-    //         match code {
-    //             KeyCode::Char(c) => {
-    //                 app.chars.insert(*&app.index as usize, *&c);
-    //                 app.index += 1;
-    //
-    //                 if x_last && y_last { execute!(stdout(), ScrollUp(1)); };
-    //                 if x_last {
-    //                     execute!(stdout(), MoveToNextLine(1));
-    //                     app.chars.insert(app.index as usize, '\n');
-    //                     app.index += 1;
-    //                 };
-    //                 execute!(stdout(), Print(c));
-    //             },
-    //             KeyCode::Enter => {
-    //                 if y_last { execute!(stdout(), ScrollUp(1)); };
-    //                 execute!(stdout(), MoveToNextLine(1));
-    //                 app.chars.insert(app.index as usize, '\n');
-    //                 app.index += 1;
-    //             },
-    //             KeyCode::Left => {
-    //                 if x == 0 && y == 0 && app.index == 0 { continue; };
-    //                 if x == 0 && y == 0 { execute!(stdout(), ScrollDown(1)); }
-    //                 if x == 0 {
-    //                     app.index -= 1;
-    //                     execute!(stdout(), MoveToPreviousLine(1));
-    //                     let previous_line : Option<String> = app.get_before_line(&width);
-    //                     if previous_line.is_some() {
-    //                         execute!(stdout(), Print(previous_line.unwrap()));
-    //                     }
-    //                 }
-    //                 else {
-    //                     execute!(stdout(), MoveLeft(1));
-    //                     app.index -= 1;
-    //                 };
-    //             },
-    //             KeyCode::Right => {
-    //                 let current: Option<&char> = app.chars.get(app.index as usize);
-    //                 if current.is_none() { continue; };
-    //                 let current: char = *current.unwrap();
-    //                 let current_is_lf: bool = current == '\n';
-    //                 if x_last || current_is_lf {
-    //                     app.index += 1;
-    //                     if y_last { execute!(stdout(), ScrollUp(1)); }
-    //                     execute!(stdout(), MoveToNextLine(1));
-    //                     let displays : String = app.get_element_after_cursor(&width, &x).unwrap_or(String::from(""));
-    //                     if displays.is_empty() { continue; };
-    //                     execute!(stdout(), Clear(ClearType::CurrentLine), Print(&displays));
-    //                     let y : u16 = terminal.get_cursor().unwrap().1;
-    //                     execute!(stdout(), MoveTo(0, y));
-    //                 } else {
-    //                     execute!(stdout(), MoveRight(1));
-    //                     app.index += 1;
-    //                 };
-    //             }
-    //             KeyCode::Backspace => {
-    //                 //
-    //             },
-    //             _ => (),
-    //         }
-    //
-    //         //debug
-    //         //display_coordinate(&mut terminal, &app);
-    //     }
-    // }
-    //execute!(stdout(), LeaveAlternateScreen);
+    execute!(stdout(), LeaveAlternateScreen);
     disable_raw_mode()?;
 
     //debug
